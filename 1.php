4f9a95c89c88aea5b39994e333f40770c304e133
gabest <gabest11@gmail.com>
2020-12-21 04:03:07 +0100

    S02E01 more fixes

--- 1.php
+++ 1.php
@@ -4,9 +4,74 @@ if(!file_exists($argv[1])) die('check file name');
 
 $title = preg_replace('/\\.[^\\.]+$/i', '', $argv[1]);
 
+function sanitycheck($title, &$bogusframes)
+{
+	// find auto-deinterlaced frames that should be progressive
+
+	$frames = [];
+
+	foreach(explode("\n", file_get_contents("$title-tfm-ovr.txt")) as $row)
+	{
+		$row = trim($row);
+	
+		if(preg_match('/^([0-9]+)(,([0-9]+))? +([cpbnu]+)/i', $row, $m))
+		{
+			if(!empty($m[2])) 
+			{
+				// should be one for every "scene change", but we basically mark every single one of them
+				
+				$frames[$m[1]]['first'] = true; 
+			}
+			
+			if(empty($m[3])) $m[3] = $m[1];
+		
+			for($i = (int)$m[1], $j = (int)$m[3], $k = 0; $i <= $j; $i++)
+			{
+				$frames[$i]['type']['value'] = $m[4][$k];
+				$frames[$i]['type']['row'] = $row;
+		
+				if(++$k == strlen($m[4])) $k = 0;
+			}
+		}
+	
+		if(preg_match('/^([0-9]+)(,([0-9]+))? +([\\+\\-]+)/i', $row, $m))
+		{
+			if(empty($m[3])) $m[3] = $m[1];
+		
+			for($i = (int)$m[1], $j = (int)$m[3], $k = 0; $i <= $j; $i++)
+			{
+				$frames[$i]['deint']['value'] = $m[4][$k];
+				$frames[$i]['deint']['row'] = $row;
+		
+				if(++$k == strlen($m[4])) $k = 0;
+			}
+		}
+	}
+
+	$bogusframes = [];
+
+	foreach(explode("\n", file_get_contents("$title-tfm.txt")) as $row)
+	{
+		$row = trim($row);
+	
+		if(!preg_match('/^([0-9]+) ([cpbnuhl]) \\+/i', $row, $m)) continue;
+	
+		$i = (int)$m[1];
+	
+		if(!isset($frames[$i]['type']) 
+		|| isset($frames[$i]['deint']) && $frames[$i]['deint']['value'] == '+' 
+		|| isset($frames[$i]['first']))
+		{
+			continue;
+		}
+	
+		$bogusframes[$i] = $frames[$i];
+	}
+}
+
 $cthresh = 9;
-$MI = isset($argv[2]) ? (int)$argv[2] : 80;
-$PP = isset($argv[3]) ? (int)$argv[3] : 6;
+$MI = 80;
+$PP = 6;
 
 // avs
 
@@ -31,8 +96,7 @@ TDecimate(mode=4,denoise=true,output="$title-tdec.txt")
 crop(344,224,-344,-224)
 EOT;
 
-file_put_contents($title.'-1.avs', $avs);
-
+if(!file_exists("$title-1.avs")) file_put_contents("$title-1.avs", $avs);
 if(!file_exists("$title-tfm-ovr.txt")) file_put_contents("$title-tfm-ovr.txt", '');
 if(!file_exists("$title-tdec-ovr.txt")) file_put_contents("$title-tdec-ovr.txt", '');
 //if(!file_exists("$title-tdeint-ovr.txt")) file_put_contents("$title-tdeint-ovr.txt", '');
@@ -45,9 +109,44 @@ EOT;
 
 $cmd = preg_replace('/[\r\n]+/', ' ', $cmd);
 
-$ret = 0;
-passthru($cmd, $ret);
-if(!empty($ret)) die($ret);
+if(!file_exists("$title-tfm.txt") || !file_exists("$title-tdec.txt"))
+{
+	echo $cmd.PHP_EOL;
+	
+	$ret = 0;
+	passthru($cmd, $ret);
+	if(!empty($ret)) die($ret);
+}
+
+// sanity check
+
+$bogusframes = [];
+
+sanitycheck($title, $bogusframes);
+
+$rows = [];
+
+$typerow = '';
+
+foreach($bogusframes as $i => $bf)
+{
+	if($typerow != $bf['type']['row'])
+	{
+		if(!empty($rows)) $rows[] = '';
+		$rows[] = $bf['type']['row'].(isset($bf['deint']['row']) ? PHP_EOL.$bf['deint']['row'] : '');
+		$rows[] = '';
+		
+		$typerow = $bf['type']['row'];
+	}
+	
+	$s = ' '.$i.' '.$bf['type']['value'];
+	
+	if(isset($bf['deint'])) $s .= ' '.$bf['deint']['value'];
+	
+	$rows[] = $s;
+}
+
+file_put_contents("$title-bogusframes.txt", implode(PHP_EOL, $rows));
 
 // 2nd pass
 
@@ -61,8 +160,7 @@ TFM(d2v=d2vpath,clip2=deint,mode=0,slow=2,cthresh=$cthresh,MI=$MI,PP=$PP,chroma=
 TDecimate(mode=5,hybrid=2,denoise=true,vfrDec=0,input="$title-tdec.txt",tfmIn="$title-tfm.txt",mkvOut="$title-timecodes.txt",ovr="$title-tdec-ovr.txt")
 EOT;
 
-file_put_contents($title.'-2.avs', $avs);
-
+if(!file_exists("$title-2.avs")) file_put_contents("$title-2.avs", $avs);
 if(!file_exists("$title-tfm-ovr.txt")) file_put_contents("$title-tfm-ovr.txt", '');
 if(!file_exists("$title-tdec-ovr.txt")) file_put_contents("$title-tdec-ovr.txt", '');
 //if(!file_exists("$title-tdeint-ovr.txt")) file_put_contents("$title-tdeint-ovr.txt", '');
@@ -78,9 +176,15 @@ EOT;
 
 $cmd = preg_replace('/[\r\n]+/', ' ', $cmd);
 
-$ret = 0;
-passthru($cmd, $ret);
-if(!empty($ret)) die($ret);
+if(file_exists("$title-tfm.txt") && file_exists("$title-tdec.txt") 
+&& (!file_exists("$title-timecodes.txt") || !file_exists("$title-huffyuv.mkv")))
+{
+	echo $cmd.PHP_EOL;
+	
+	$ret = 0;
+	passthru($cmd, $ret);
+	if(!empty($ret)) die($ret);
+}
 
 // ranges
 
