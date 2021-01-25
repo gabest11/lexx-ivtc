3da19ee61813b15f35f72e15e3631b287f107ffa
gabest11 <gabest11@gmail.com>
2021-01-25 01:25:54 +0100

    S01E02 wip

--- 0.bat
+++ 0.bat
@@ -1,2 +1,2 @@
 ffmpeg -hide_banner -i %1 -c copy -f mpeg2video %~n1.es
-E:\tmp\media\util\dgmpgdec158.bak\DGIndex.exe -i %~n1.es -o %~n1 -exit
+E:\tmp\media\util\AvisynthRepository_361\AVSPLUS_x86\plugins\DGIndex.exe -i %~n1.es -o %~n1 -exit
