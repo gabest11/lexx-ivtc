e5e0be163c7640ff417fa730ea373f92f7d47552
gabest11 <gabest11@gmail.com>
2021-03-26 22:47:12 +0100

    2.php ignore 1pass extra frames

--- 2.php
+++ 2.php
@@ -148,6 +148,12 @@ if(!empty($keyframes)) $cmd[] = '-force_key_frames '.$keyframes;
 $cmd[] = '-metadata title="'.$title.'"';
 $cmd[] = '-metadata description="https://github.com/gabest11/lexx-ivtc" ';
 $cmd[] = '-metadata:s title= ';
+
+if(!empty($timecodes) && preg_match('/# TDecimate Mode [0-9]+: +Last Frame = ([0-9]+)/i', file_get_contents($timecodes), $m))
+{
+	$cmd[] = '-frames:v '.($m[1] + 1);
+}
+
 $cmd[] = '"'.$dst.'"';
 
 // TODO: chapters for keyframes
