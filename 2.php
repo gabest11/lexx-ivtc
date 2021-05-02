c1529f5c1ed4c8819804e8cfd0cb421d1fe4b96e
gabest11 <gabest11@gmail.com>
2021-05-02 13:28:24 +0200

    S01E01 jerky motion scene fixes

--- 2.php
+++ 2.php
@@ -199,7 +199,8 @@ $cmd = [];
 $cmd[] = 'ffmpeg -hide_banner';
 if($resolution[1] >= 720) $cmd[] = '-colorspace bt709';
 if(preg_match('/^(.+):([0-9]+)$/i', $src, $m)) {$src = $m[1];} // $cmd[] = '-start_number '.$m[2];} // changing the start isn't compatible with the vfr timecode file
-$cmd[] = '-r 30000/1001 -i "'.$src.'"';
+if(strpos($src, "%") !== false) $cmd[] = '-r 30000/1001';
+$cmd[] = '-i "'.$src.'"';
 foreach($audio as $a) $cmd[] = ($a['is51'] ? '-channel_layout 5.1 ' : '').'-i "'.$a['fn'].'"';
 foreach($subtitle as $s) $cmd[] = '-i "'.$s['fn'].'"';
 if(strpos($codec, 'p10') !== false) $cmd[] = '-pix_fmt yuv420p10le';
